6a16a6fc18de32c71d8bee8b3a11a48e7f866103
qwe940723 <qwe940723@gmail.com>
2025-06-21 18:11:35 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;           
+using System.IO;              
 
 namespace SimpleClock
 {
@@ -15,6 +17,106 @@ namespace SimpleClock
         public Form1()
         {
             InitializeComponent();
+
+            comboboxInitialzation();  
+
+            timerClcok.Start();       
+        }
+        List<string> hours = new List<string>();            
+        List<string> minutes = new List<string>();          
+
+        string strSelectTime = "";                          
+
+        private WaveOutEvent waveOut;                     
+        private AudioFileReader audioFileReader;            
+
+
+        private void comboboxInitialzation()
+        {
+            
+            for (int i = 0; i <= 23; i++)
+                cmbHour.Items.Add(string.Format("{0:00}", i));
+            cmbHour.SelectedIndex = 0;
+
+            
+            for (int i = 0; i <= 59; i++)
+                cmbMin.Items.Add(string.Format("{0:00}", i));
+            cmbMin.SelectedIndex = 0;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerClcok_Tick(object sender, EventArgs e)
+        {
+            txtTime.Text = DateTime.Now.ToString("HH:mm:ss");    
+            txtDate.Text = DateTime.Now.ToString("yyyy-MM-dd"); 
+            txtWeekDay.Text = DateTime.Now.ToString("dddd");     
+        }
+
+        private void timerAlert_Tick(object sender, EventArgs e)
+        {
+           
+            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
+            {
+                try
+                {
+                    stopWaveOut();
+
+                    
+                    string audioFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "alert.wav");
+
+                   
+                    audioFileReader = new AudioFileReader(audioFilePath);
+
+                    
+                    waveOut = new WaveOutEvent();
+                    waveOut.Init(audioFileReader);
+
+                   
+                    waveOut.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法播放聲音檔，錯誤資訊: " + ex.Message);
+                }
+                finally
+                {
+                    timerAlert.Stop(); 
+                }
+            }
+        }
+        private void stopWaveOut()
+        {
+            
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+
+        private void btnSetAlert_Click(object sender, EventArgs e)
+        {
+            timerAlert.Start(); 
+            btnSetAlert.Enabled = false;
+            btnCancelAlert.Enabled = true;
+            strSelectTime = cmbHour.SelectedItem.ToString() + ":" + cmbMin.SelectedItem.ToString(); 
+        }
+
+        private void btnCancelAlert_Click(object sender, EventArgs e)
+        {
+            stopWaveOut();    
+            timerAlert.Stop(); 
+            btnSetAlert.Enabled = true;
+            btnCancelAlert.Enabled = false;
         }
     }
 }
